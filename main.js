3024ecd77df53a764b9d287ce31e25875192f295
Teri11 <augustineteri157@gmail.com>
2022-06-24 15:11:15 +0300

    modified strings in scripts

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 // alert("Welcome to Introduction to Javascript");
+
+// STRINGS
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+let text = "John Doe";
